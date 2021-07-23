b7dab0963da5c07942d678922da22cab438d45e0
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-07-23 10:56:23 +0200

    adding todo

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const addTodo = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const error = document.querySelector('.error');
+
+// genrating function for create todo html
+const generateTemplate = (newTodo) => {
+  const html = `
+  <li class="list-group-item d-flex justify-content-between   align-items-center text-light">
+    <span>${newTodo}</span>
+    <i class="far fa-trash-alt delete"></i>
+  </li>`;
+
+  list.innerHTML += html;
+};
+
+addTodo.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // trim cut all white space
+  const newTodo = addTodo.add.value.trim();
+  // console.log(newTodo);
+
+  if (newTodo.length) {
+    // generate template
+    generateTemplate(newTodo);
+    error.classList.add('d-none');
+    addTodo.reset();
+  } else {
+    error.classList.remove('d-none');
+  }
+});
